df7f13f3ac395484b5558f2178f0f207305b8641
Lukasz Blaszkowski <lblaszkowski@onet.pl>
2020-01-27 10:46:50 +0100

    zadanie 4

--- test_login_to_the_panel.py
+++ test_login_to_the_panel.py
@@ -1,22 +1,20 @@
 import pytest
 import os
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
 
 
 @pytest.fixture
 def driver(request, browser="ch"):
     if browser == "chrome" or browser == "ch":
-        wd = webdriver.Chrome(executable_path=r'Drivers/CH/chromedriver.exe')
+        wd = webdriver.Chrome(executable_path=r'Driver/CH/chromedriver.exe')
         # request.addfinalizer(wd.quit)
     elif browser == "mozilla" or browser == "ff":
         wd = driver = webdriver.Firefox(executable_path=r'Driver/FF/geckodriver.exe')
         # request.addfinalizer(wd.quit)
     else:
-        wd = driver = webdriver.Ie(executable_path=r'../Drivers/IE_11/IEDriverServer.exe')
-        wd = driver.maximize_window()
+        wd = driver = webdriver.Ie(executable_path=r'Driver/IE11/IEDriverServer.exe')
         IE_Brows = os.path.dirname(__file__)
-        ie_driver_path = IE_Brows + "../Drivers/IE11/IEDriverServer.exe"
+        ie_driver_path = IE_Brows + "Driver/IE11/IEDriverServer.exe"
         # request.addfinalizer(wd.quit)
     return wd
 
