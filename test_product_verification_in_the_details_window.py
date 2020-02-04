94d4d1b9dcf2ef77c18685541f68b9c546786a44
Lukasz Blaszkowski <lblaszkowski@onet.pl>
2020-02-04 18:39:37 +0100

    zadanie 10 -- jest ok

--- test_product_verification_in_the_details_window.py
+++ test_product_verification_in_the_details_window.py
@@ -2,6 +2,8 @@ import pytest
 from selenium import webdriver
 import time
 
+
+
 @pytest.fixture
 def driver(request, browser="ff"):
     if browser == "chrome" or browser == "ch":
@@ -19,10 +21,30 @@ def driver(request, browser="ff"):
 def test_loading_page(driver):
     driver.get("http://localhost/litecart/")
     name_product = driver.find_element_by_xpath("//div[contains(@id,'box-campaigns')]//ul[contains(@class,'listing-wrapper')]//li//div[starts-with(@class,'name')]").text
-    print(name_product)
-    price_name = driver.find_element_by_xpath("//div[contains(@id,'box-campaigns')]//ul[contains(@class,'listing-wrapper')]//li//strong[starts-with(@class,'campaign-price')]").text
+    price_name = driver.find_element_by_xpath("//div[contains(@id,'box-campaigns')]//ul[contains(@class,'listing-wrapper')]//li//s[starts-with(@class,'regular-price')]").text
     print(price_name)
+    price_name_color = driver.find_element_by_xpath("//div[contains(@id,'box-campaigns')]//s").value_of_css_property("color")
+    print(price_name_color)
+    price_promotion_name = driver.find_element_by_xpath("//div[contains(@id,'box-campaigns')]//ul[contains(@class,'listing-wrapper')]//li//strong[starts-with(@class,'campaign-price')]").text
+    price_promotion_color = driver.find_element_by_xpath("//div[contains(@id,'box-campaigns')]//strong[starts-with(@class,'campaign-price')]").value_of_css_property("color")
     driver.execute_script("window.scrollTo(0, 500)")
     product = driver.find_element_by_xpath("//div[contains(@id,'box-campaigns')]//ul[contains(@class,'listing-wrapper')]//li")
-    product.click
-    time.sleep(5)
+    product.click()
+    verification_name_product = driver.find_element_by_xpath("//div[contains(@class,'content')]//h1[contains(@class,'title')]").text
+    verification_price_name = driver.find_element_by_xpath("//div[contains(@class,'content')]//s[contains(@class,'regular-price')]").text
+    verification_price_color = driver.find_element_by_xpath("//div[contains(@class,'content')]//s[contains(@class,'regular-price')]").value_of_css_property("color")
+    verification_price_promotion_name = driver.find_element_by_xpath("//div[contains(@class,'content')]//strong[contains(@class,'campaign-price')]").text
+    verification_price_promotion_color = driver.find_element_by_xpath("//div[contains(@class,'content')]//strong[contains(@class,'campaign-price')]").value_of_css_property("color")
+
+    print(name_product, "==", verification_name_product)
+    print(price_name, "==", verification_price_name)
+    print(price_name_color, "==", verification_price_color)
+    print(price_promotion_name, "==", verification_price_promotion_name)
+    print(price_promotion_color, "==", verification_price_promotion_color)
+
+    assert (price_name_color == verification_price_promotion_color), f"{price_name_color} ':' {verification_price_promotion_color} - kolor ceny podstawowej są różne"
+    assert (price_promotion_color == verification_price_color), f"{price_promotion_color} ':' {verification_price_color} - kolor ceny promocyjnej są różne"
+    assert (name_product == verification_name_product), f"{name_product} ':' {verification_name_product} - nazwy produktów są różne"
+    assert (price_name == verification_price_name), f"{price_name} ':' {verification_price_name} - kwoty podstawowe są różne"
+    assert (price_promotion_name == verification_price_promotion_name), f"{price_promotion_name} ':' {verification_price_promotion_name} - kwoty promocyjne są różne"
+
