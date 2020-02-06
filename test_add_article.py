83a7eaad50732a302809ef0ab8cf9eeba2075d67
Lukasz Blaszkowski <lblaszkowski@onet.pl>
2020-02-06 19:39:04 +0100

    zadanie 12 - problem z dodaniem image

--- test_add_article.py
+++ test_add_article.py
@@ -25,6 +25,7 @@ def test_loading_page(driver):
     driver.find_element_by_name("login").click()
     driver.find_element_by_xpath("//li[contains(@id,'app')]//span[text()='Catalog']").click()
     driver.find_element_by_xpath("//*[@id='content']/div[1]/a[1]/i").click()
+    driver.find_element_by_name("name[en]").send_keys("Kaczka")
     driver.find_element_by_name("code").send_keys("1234")
     Parent_Category = Select(driver.find_element_by_name("parent_id"))
     Parent_Category.select_by_value("1")
@@ -36,6 +37,8 @@ def test_loading_page(driver):
     add_image = driver.find_element_by_name("image")
     add_image.click()
     add_image.send_keys("C:/training_with_selenium_python/image/rodzinnespotkania.jpeg")
-
-
+    driver.find_element_by_name("priority").send_keys("123")
+    driver.find_element_by_xpath("//div[contains(@id,'content-wrapper')]//a[text()='Information']").click()
+    driver.find_element_by_name("h1_title[en]").send_keys("Kaczka")
+    driver.find_element_by_name("short_description[en]").send_keys("Opis -- Kaczka")
 
