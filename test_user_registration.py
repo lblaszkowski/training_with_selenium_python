c07503b9b04f0d91e4b5a1cefa57442271bd51e8
Lukasz Blaszkowski <lblaszkowski@onet.pl>
2020-02-06 20:42:39 +0100

    zadanie 11 - ok

--- test_user_registration.py
+++ test_user_registration.py
@@ -22,7 +22,7 @@ def test_loading_page(driver):
     driver.get("http://localhost/litecart/")
     driver.find_element_by_xpath("//a[text()='New customers click here']").click()
     tax_id = driver.find_element_by_name("tax_id")
-    tax_id.send_keys("1234")
+    tax_id.send_keys("")
     company = driver.find_element_by_name("company")
     company.send_keys("Polska")
     firstname = driver.find_element_by_name("firstname")
@@ -36,7 +36,7 @@ def test_loading_page(driver):
     city = driver.find_element_by_name("city")
     city.send_keys("Gdańsk")
     email = driver.find_element_by_name("email")
-    email.send_keys("marcelina.kos@interia.pl")
+    email.send_keys("marcelina.kos111@we.pl")
     phone = driver.find_element_by_name("phone")
     phone.send_keys("123456789")
     password = driver.find_element_by_name("password")
@@ -44,9 +44,15 @@ def test_loading_page(driver):
     confirmed_password = driver.find_element_by_name("confirmed_password")
     confirmed_password.send_keys("Legia09aa")
     driver.find_element_by_name("create_account").click()
-    time.sleep(10)
-
-
+    Logout_klick = driver.find_element_by_xpath("//div[contains(@class,'content')]//ul[contains(@class,'list-vertical')]//a[text()='Logout']")
+    Logout_klick.click()
+    email = driver.find_element_by_name("email")
+    email.send_keys("marcelina.kos@interia.pl")
+    password = driver.find_element_by_name("password")
+    password.send_keys("Legia09aa")
+    driver.find_element_by_name("login").click()
+    Logout_klick = driver.find_element_by_xpath("//div[contains(@class,'content')]//ul[contains(@class,'list-vertical')]//a[text()='Logout']")
+    Logout_klick.click()
 
 
 
