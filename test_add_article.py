d514c52ea3400eee1b2b65e6a16311e23709c2f4
Lukasz Blaszkowski <lblaszkowski@onet.pl>
2020-02-07 15:39:15 +0100

    zadanie 12 -- dodanie artykułu - ok

--- test_add_article.py
+++ test_add_article.py
@@ -17,29 +17,30 @@ def driver(request, browser="ff"):
     return wd
 
 
-
-def test_loading_page(driver):
+def test_loading_page(driver, name_file='Kaczkaasas'):
     driver.get("http://localhost/litecart/admin/")
     driver.find_element_by_name("username").send_keys("admin")
     driver.find_element_by_name("password").send_keys("admin")
     driver.find_element_by_name("login").click()
     driver.find_element_by_xpath("//li[contains(@id,'app')]//span[text()='Catalog']").click()
-    driver.find_element_by_xpath("//*[@id='content']/div[1]/a[1]/i").click()
-    driver.find_element_by_name("name[en]").send_keys("Kaczka")
-    driver.find_element_by_name("code").send_keys("1234")
-    Parent_Category = Select(driver.find_element_by_name("parent_id"))
-    Parent_Category.select_by_value("1")
-    driver.find_element_by_name("google_taxonomy_id")
-    driver.find_element_by_name("dock[]")
-    List_Style = Select(driver.find_element_by_name("list_style"))
-    List_Style.select_by_value("rows")
-    driver.find_element_by_name("keywords")
-    add_image = driver.find_element_by_name("image")
-    add_image.click()
-    add_image.send_keys("C:/training_with_selenium_python/image/rodzinnespotkania.jpeg")
-    driver.find_element_by_name("priority").send_keys("123")
-    driver.find_element_by_xpath("//div[contains(@id,'content-wrapper')]//a[text()='Information']").click()
-    driver.find_element_by_name("h1_title[en]").send_keys("Kaczka")
-    driver.find_element_by_name("short_description[en]").send_keys("Opis -- Kaczka")
-
-
+    driver.find_element_by_xpath("//*[@id='content']/div[1]/a[2]/i").click()
+    driver.find_element_by_name("name[en]").send_keys(name_file)
+    driver.find_element_by_name("code").send_keys("123")
+    driver.find_element_by_name("categories[]").click()
+    if driver.find_element_by_xpath("//*[@id='tab-general']/table/tbody/tr[4]/td/div/table/tbody/tr[2]/td[1]/input").is_displayed():
+        driver.find_element_by_xpath("//*[@id='tab-general']/table/tbody/tr[4]/td/div/table/tbody/tr[2]/td[1]/input").click()
+    driver.find_element_by_name("product_groups[]").click()
+    quantity = driver.find_element_by_name("quantity")
+    quantity.click()
+    quantity.send_keys("10")
+    quantity_unit_id = Select(driver.find_element_by_name("quantity_unit_id"))
+    quantity_unit_id.select_by_value("1")
+    delivery_status_id = Select(driver.find_element_by_name("delivery_status_id"))
+    delivery_status_id.select_by_value("1")
+    sold_out_status_id = Select(driver.find_element_by_name("sold_out_status_id"))
+    sold_out_status_id.select_by_value("1")
+    driver.find_element_by_name("date_valid_from").click()
+    driver.find_element_by_name("date_valid_to").click()
+    driver.find_element_by_name("save").click()
+    name_file_product = driver.find_element_by_xpath("//tr[contains(@class,'row')]//a[text()='" + name_file + "']")
+    assert (name_file_product.text == name_file), f"{name_file_product} ':' {name_file} - stworzona nazwa produktu jest różna"
