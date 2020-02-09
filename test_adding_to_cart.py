1ce101d64d76f969fdf5dbac207818990d8cee30
Lukasz Blaszkowski <lblaszkowski@onet.pl>
2020-02-09 23:35:46 +0100

    zadanie 13 - dodanie do koszyka -- ok

---
+++ test_adding_to_cart.py
@@ -0,0 +1,51 @@
+import pytest
+from selenium import webdriver
+import time
+from selenium.webdriver.support.ui import Select
+
+
+@pytest.fixture
+def driver(request, browser="ff"):
+    if browser == "chrome" or browser == "ch":
+        wd = webdriver.Chrome(executable_path=r'Driver/CH/chromedriver.exe')
+        # wd = driver.maximize_window()
+        request.addfinalizer(wd.quit)
+    else:
+        wd = webdriver.Firefox(executable_path=r'Driver/FF_026/geckodriver.exe')
+        # wd = driver.maximize_window()
+        request.addfinalizer(wd.quit)
+    return wd
+
+
+def test_adding_to_cart_in_page(driver, name_file='Kaczkaasas'):
+    driver.get("http://localhost/litecart/en/")
+    driver.find_element_by_xpath("//div[contains(@id,'box-campaigns')]//ul[contains(@class,'listing-wrapper')]//li//div[starts-with(@class,'name')]").click()
+    options_Size = Select(driver.find_element_by_name("options[Size]"))
+    options_Size.select_by_value("Small")
+    driver.find_element_by_name("add_cart_product").click()
+    time.sleep(1)
+    driver.find_element_by_class_name("general-0").click()
+    driver.find_element_by_xpath("//div[contains(@id,'box-campaigns')]//ul[contains(@class,'listing-wrapper')]//li//div[starts-with(@class,'name')]").click()
+    options_Size = Select(driver.find_element_by_name("options[Size]"))
+    options_Size.select_by_value("Small")
+    driver.find_element_by_name("add_cart_product").click()
+    time.sleep(1)
+    driver.find_element_by_class_name("general-0").click()
+    driver.find_element_by_xpath("//div[contains(@id,'box-campaigns')]//ul[contains(@class,'listing-wrapper')]//li//div[starts-with(@class,'name')]").click()
+    options_Size = Select(driver.find_element_by_name("options[Size]"))
+    options_Size.select_by_value("Small")
+    driver.find_element_by_name("add_cart_product").click()
+    time.sleep(1)
+    driver.find_element_by_class_name("general-0").click()
+    driver.find_element_by_xpath("//a[text()='Checkout »']").click()
+    update = driver.find_element_by_name("quantity")
+    update.clear()
+    update.send_keys("2")
+    driver.find_element_by_name("update_cart_item").click()
+    time.sleep(1)
+    update = driver.find_element_by_name("quantity")
+    update.clear()
+    update.send_keys("1")
+    driver.find_element_by_name("update_cart_item").click()
+    time.sleep(1)
+    driver.find_element_by_name("remove_cart_item").click()
