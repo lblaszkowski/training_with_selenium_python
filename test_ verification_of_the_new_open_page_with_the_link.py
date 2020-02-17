0668344a20b33fb79f0132a0a3ac21d80864590c
Lukasz Blaszkowski <lblaszkowski@onet.pl>
2020-02-17 12:47:36 +0100

    zadanie 15 - problem z wybraniem drugiej ikonki

--- test_ verification_of_the_new_open_page_with_the_link.py
+++ test_ verification_of_the_new_open_page_with_the_link.py
@@ -1,10 +1,6 @@
 import pytest
 from selenium import webdriver
 import time
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC, expected_conditions
-from selenium.webdriver.common.keys import Keys
 
 
 
@@ -26,20 +22,17 @@ def test_verification_of_the_new_open_page_with_the_link_page(driver):
     driver.find_element_by_name("username").send_keys("admin")
     driver.find_element_by_name("password").send_keys("admin")
     driver.find_element_by_name("login").click()
+    time.sleep(1)
     driver.find_element_by_xpath("//li[contains(@id,'app')]//span[text()='Countries']").click()
-    time.sleep(5)
+    time.sleep(1)
     driver.find_element_by_xpath("//table[contains(@class,'dataTable')]//a[text()='Afghanistan']").click()
-    main_window = driver.current_window_handle
-    time.sleep(5)
-    first_link = driver.find_element_by_xpath("//a[contains(@href,'ISO_3166-1_alpha-2')]")
-    first_link.send_keys(Keys.CONTROL + Keys.RETURN)
-    time.sleep(5)
-    link = driver.find_element_by_xpath("//body[contains(@class,'mediawiki ltr sitedir-ltr mw-hide-empty-elt ns-0 ns-subject mw-editable page-ISO_3166-1_alpha-2 rootpage-ISO_3166-1_alpha-2 skin-vector action-view')]")
-    link.send_keys(Keys.CONTROL + Keys.TAB)
+    driver.find_element_by_xpath("//a[contains(@href,'ISO_3166-1_alpha-2')]").click()
+    driver.switch_to.window(driver.window_handles[1])
+    driver.close()
+    time.sleep(2)
+    driver.find_element_by_xpath("//a[contains(@href,'ISO_3166-1_alpha-3')]").click()
+    driver.switch_to.window(driver.window_handles[0])
+    time.sleep(1)
+    driver.close()
+    time.sleep(1)
 
-    # alpha_2 = WebDriverWait(driver, 100).until(
-    #     EC.element_to_be_clickable((By.CLASS_NAME, "page-ISO_3166-1_alpha-2")))
-    # alpha_2.send_keys(Keys.CONTROL + Keys.TAB)
-
-    # driver.find_element_by_tag_name("body").send_keys(Keys.CONTROL + Keys.TAB)
-    # driver.switch_to_window(main_window)
