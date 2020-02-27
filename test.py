3423ddbf143de9b76d7a9669df2d548af5586ec9
Lukasz Blaszkowski <lblaszkowski@onet.pl>
2020-02-27 13:32:26 +0100

    zadanie 13 - poprawka ok

---
+++ test.py
@@ -0,0 +1,5 @@
+a = 0
+
+while a != 4:
+    print(a)
+    a += 1
--- test_adding_to_cart.py
+++ test_adding_to_cart.py
@@ -15,30 +15,21 @@ def driver(request, browser="ff"):
     return wd
 
 
-def test_adding_to_cart_in_page(driver):
+def test_adding_to_cart_in_page(driver, addToCart = 0, count=3):
     driver.get("http://localhost/litecart/en/")
-    driver.find_element_by_xpath("//div[contains(@id,'box-campaigns')]//ul[contains(@class,'listing-wrapper')]//li//div[starts-with(@class,'name')]").click()
-    options_Size = Select(driver.find_element_by_name("options[Size]"))
-    options_Size.select_by_value("Small")
-    driver.find_element_by_name("add_cart_product").click()
-    time.sleep(1)
-    driver.find_element_by_class_name("general-0").click()
-    driver.find_element_by_xpath("//div[contains(@id,'box-campaigns')]//ul[contains(@class,'listing-wrapper')]//li//div[starts-with(@class,'name')]").click()
-    options_Size = Select(driver.find_element_by_name("options[Size]"))
-    options_Size.select_by_value("Small")
-    driver.find_element_by_name("add_cart_product").click()
-    time.sleep(1)
-    driver.find_element_by_class_name("general-0").click()
-    driver.find_element_by_xpath("//div[contains(@id,'box-campaigns')]//ul[contains(@class,'listing-wrapper')]//li//div[starts-with(@class,'name')]").click()
-    options_Size = Select(driver.find_element_by_name("options[Size]"))
-    options_Size.select_by_value("Small")
-    driver.find_element_by_name("add_cart_product").click()
-    time.sleep(1)
-    driver.find_element_by_class_name("general-0").click()
+    while addToCart != count:
+        driver.find_element_by_xpath(
+            "//div[contains(@id,'box-campaigns')]//ul[contains(@class,'listing-wrapper')]//li//div[starts-with(@class,'name')]").click()
+        options_Size = Select(driver.find_element_by_name("options[Size]"))
+        options_Size.select_by_value("Small")
+        driver.find_element_by_name("add_cart_product").click()
+        time.sleep(1)
+        driver.find_element_by_class_name("general-0").click()
+        addToCart += 1
     driver.find_element_by_xpath("//a[text()='Checkout »']").click()
     update = driver.find_element_by_name("quantity")
     update.clear()
-    update.send_keys("2")
+    update.send_keys(addToCart)
     driver.find_element_by_name("update_cart_item").click()
     time.sleep(1)
     update = driver.find_element_by_name("quantity")
@@ -47,3 +38,5 @@ def test_adding_to_cart_in_page(driver):
     driver.find_element_by_name("update_cart_item").click()
     time.sleep(1)
     driver.find_element_by_name("remove_cart_item").click()
+
+
